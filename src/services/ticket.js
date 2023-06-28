5f7d502e5b890f05bffee1c0314ed6cfe5e704e3
cauamillersjc <caua.millersantos@gmail.com>
2023-06-28 12:59:25 -0300

    feat: Add finalize ticket method with barcode scanner

--- src/services/ticket.js
+++ src/services/ticket.js
@@ -1,35 +1,60 @@
 import database from "./database";
 import dateFormat from "../utils/dateFormat";
+import moment from "moment";
+import Toast from 'react-native-toast-message';
 
 export const newTicket = async (plate) => {
-    const entrance = new Date();
+    const entrance = moment();
     const id = await database.lastTicketId();
     const code = generateTicketCode(entrance);
     let ticket;
 
-    if(id == 0){
-        ticket = await database.insertTicket(code, entrance, plate, true);
+    if (id == 0) {
+        ticket = await database.insertTicket(code, entrance.format('YYYY-MM-DD HH:mm:ss'), plate, true);
     }
-    else{
-        ticket = await database.insertTicket(code, entrance, plate, false);
+    else {
+        ticket = await database.insertTicket(code, entrance.format('YYYY-MM-DD HH:mm:ss'), plate, false);
     }
 
     // ToDo: Implement method to thermal printer
 
     console.log(`
         Código: ${code}\n
-        Entrada: ${entrance.toLocaleString('pt-BR')}\n
+        Entrada: ${entrance.format('DD/MM/YYYY HH:mm:ss')}\n
         Placa: ${plate}
     `);
 
     console.log(ticket);
 }
 
+export const finalizeTicket = async (code) => {
+    const ticket = await database.getTicketByCode(code);
+    if (ticket) {
+        const entrance = moment(ticket.entrance);
+        const exit = moment();
+        const permanence = dateFormat.dateDiffInSeconds(entrance, exit);
+
+        const updatedTicket = await database.finalizeTicket(code, exit.format('YYYY-MM-DD HH:mm:ss'), permanence);
+
+        console.log(updatedTicket);
+
+        // ToDo: Implement method to thermal printer
+
+        console.log(`
+            Código: ${ticket.code}\n
+            Entrada: ${entrance.format("DD/MM/YYYY HH:mm:ss")}\n
+            Saída: ${exit.format("DD/MM/YYYY HH:mm:ss")}\n
+            Permanência: ${dateFormat.formatSecondsToHMS(permanence)}\n
+            Placa: ${ticket.plate}
+        `);
+    }
+}
+
 const generateTicketCode = (date) => {
     // O Código do ticket é composto pela data + um número aleatorio de 6 digitos. Sendo assim: YYYYmmmDDHHMMSS + 000000.
     const randomNumber = Math.floor(Math.random() * 1000000);
     const formattedNumber = randomNumber.toString().padStart(6, '0');
-    const formattedDate = dateFormat.dateToYYYYmmDDHHMMSS(date);
+    const formattedDate = date.format("YYYYMMDDHHmmss");
 
     return formattedDate + formattedNumber;
 }
